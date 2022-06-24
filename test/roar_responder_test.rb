eb46cb11ab4d9dae7ddf90af54da4e2dd7e21a26
Marta Noya <tim4martanoya@gmail.com>
2022-06-24 18:51:30 +0200

    Move integration tests folder

--- test/roar_responder_test.rb
+++ test/roar_responder_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 
-class TestRoarResponder < Minitest::Test
+class RoarResponderTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RoarResponder::VERSION
   end
