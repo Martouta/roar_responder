e2f5f391aab37bc875921de7b608b445060653c5
Marta Noya <tim4martanoya@gmail.com>
2022-06-24 17:22:26 +0200

    Test (integration) Instance Failure 'POST' of an entity (Active Record)

--- test/test_helpers/roar_responder/integration/instance/active_record_test.rb
+++ test/test_helpers/roar_responder/integration/instance/active_record_test.rb
@@ -32,10 +32,18 @@ module Integration
       end
 
       def test_post_entity_success
-        post instance_active_record_entity_path, params: {model: dummy_attrs}, as: :json
+        post instance_active_record_entity_path, params: { model: dummy_attrs }, as: :json
 
         assert_response :created
         assert_response_entity JSON.parse(response.body)
+      end
+
+      def test_post_entity_failure
+        post instance_active_record_entity_path, params: { model: dummy_attrs.merge('dummy_string' => 'invalid') },
+                                                 as: :json
+
+        assert_response :unprocessable_entity
+        assert_response_error JSON.parse(response.body)
       end
 
       private
@@ -49,13 +57,18 @@ module Integration
         response_created_at = Time.parse(response_entity['dummy']['created_at'])
         assert_in_delta Time.now, response_created_at, 2.seconds
 
-        assert_self_link(response_entity, 'dummy',  "example/#{response_id}")
+        assert_self_link(response_entity, 'dummy', "example/#{response_id}")
       end
 
       def assert_self_link(parsed_json, root_wrap, expected_url)
         self_link = parsed_json.dig(root_wrap, 'links', 0)
         assert_equal expected_url, self_link['href']
         assert_equal 'self', self_link['rel']
+      end
+
+      def assert_response_error(parsed_json)
+        expected_response = { 'errors' => { 'dummy_string' => ['invalid'] } }
+        assert_equal expected_response, parsed_json
       end
 
       def dummy_attrs
