7ef87fe8b45f9616b25f639d3786359c63c7ae5d
Marta Noya <tim4martanoya@gmail.com>
2022-06-25 15:17:34 +0200

    Test (integration) 'infer' PORO entity POST failure scenario

--- test/integration/infer/poro_test.rb
+++ test/integration/infer/poro_test.rb
@@ -18,6 +18,13 @@ module Integration
         assert_response :created
         assert_response_entity JSON.parse(response.body), 'dummy_poro_infer'
       end
+
+      def test_post_entity_failure
+        post infer_poro_index_path, params: { model: dummy_invalid_attrs }, as: :json
+
+        assert_response :unprocessable_entity
+        assert_response_error JSON.parse(response.body)
+      end
     end
   end
 end
