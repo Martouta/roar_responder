2467ad16be51c6180e755da6ad1237675b3512bd
Marta Noya <tim4martanoya@gmail.com>
2022-06-25 23:58:27 +0200

    [Refactor] DRY 'Infer' Tests

--- test/integration/infer/mongoid_test.rb
+++ test/integration/infer/mongoid_test.rb
@@ -5,46 +5,32 @@ require 'test_helper'
 module Integration
   module Infer
     class MongoidTest < ActionDispatch::IntegrationTest
-      def test_get_entity
-        entity = DummyMongoModel.create(**dummy_attrs)
+      include ::RoarResponder::TestHelpers::IntegrationTesterHelper
 
+      def setup_get_entity_request
+        @entity = DummyMongoModel.create(**dummy_attrs)
+      end
+
+      attr_reader :entity
+
+      def root_entity_wrap
+        'dummy_mongo_infer'
+      end
+
+      def perform_get_entity_request
         get infer_mongo_path(entity), as: :json
-
-        assert_response :ok
-        assert_response_entity JSON.parse(response.body), 'dummy_mongo_infer'
       end
 
-      def test_get_collection
-        collection_size = 2
-        collection_size.times { DummyMongoModel.create(**dummy_attrs) }
+      def root_collection_wrap
+        'dummy_mongo_collection_infer'
+      end
 
+      def perform_get_collection_request
         get infer_mongo_index_path, as: :json
-
-        assert_response :ok
-
-        response_collection = JSON.parse(response.body)
-
-        root_wrap = 'dummy_mongo_collection_infer'
-
-        assert_self_link(response_collection, root_wrap, '/collection')
-
-        items = response_collection.dig(root_wrap, 'items') || []
-        assert_equal collection_size, items.length
-        items.each(&method(:assert_response_entity))
       end
 
-      def test_post_entity_success
-        post infer_mongo_index_path, params: { model: dummy_attrs }, as: :json
-
-        assert_response :created
-        assert_response_entity JSON.parse(response.body), 'dummy_mongo_infer'
-      end
-
-      def test_post_entity_failure
-        post infer_mongo_index_path, params: { model: dummy_invalid_attrs }, as: :json
-
-        assert_response :unprocessable_entity
-        assert_response_error JSON.parse(response.body)
+      def create_path
+        infer_mongo_index_path
       end
     end
   end
